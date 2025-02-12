da08c90bb69654acc0fd4a1d670673039d105cbb
mattymclauchlan <info@spanishholidayrentals.co.uk>
2025-02-12 23:29:16 +0100

    update

--- functions/pricing/src/main.js
+++ functions/pricing/src/main.js
@@ -6,7 +6,7 @@ import { Client, Databases, Query } from 'node-appwrite';
 const client = new Client();
 client
   .setEndpoint(process.env.APPWRITE_ENDPOINT)
-  .setProject(process.env.APPWRITE_PROJECT_ID)
+  .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
   .setKey(process.env.APPWRITE_API_KEY);
 
 const databases = new Databases(client);

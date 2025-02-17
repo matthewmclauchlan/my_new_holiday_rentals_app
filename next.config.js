4a24276330a926813da5bbf5d289f01270084620
mattymclauchlan <info@spanishholidayrentals.co.uk>
2025-02-17 13:10:06 +0100

    update

--- next.config.js
+++ next.config.js
@@ -2,10 +2,8 @@ const { withExpo } = require('@expo/next-adapter');
 
 module.exports = withExpo({
   reactStrictMode: true,
-  // Enable the Next.js app directory
   experimental: {
     appDir: true,
   },
-  // Ensure expo-router is transpiled correctly
   transpilePackages: ['expo-router'],
 });

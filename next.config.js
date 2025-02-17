3bac43c85570f32fa7ff08a24e0f14048deaacef
mattymclauchlan <info@spanishholidayrentals.co.uk>
2025-02-17 14:28:07 +0100

    update

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ module.exports = withExpo({
   reactStrictMode: true,
   transpilePackages: ['expo-router', 'expo-modules-core', 'expo'],
   webpack: (config, { isServer }) => {
+    // Ensure .mjs is in the list of extensions to resolve
+    config.resolve.extensions.push('.mjs');
+
     // Add a rule to properly handle .mjs files in node_modules
     config.module.rules.push({
       test: /\.mjs$/,
